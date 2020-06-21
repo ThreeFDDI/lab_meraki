3075a4abd342b77eada35489d90db4793f4b21d6
JT <foosyou@gmail.com>
2020-06-21 13:26:02 -0500

    moar meraki

---
+++ m_get_switchports.py
@@ -0,0 +1,22 @@
+import meraki
+from pprint import pprint
+
+# Defining your API key as a variable in source code is not recommended
+API_KEY = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+# Instead, use an environment variable as shown under the Usage section
+# @ https://github.com/meraki/dashboard-api-python/
+
+dashboard = meraki.DashboardAPI(API_KEY)
+
+serial = 'Q2HP-Q9S8-BVHB'
+
+response = dashboard.switch_ports.getDeviceSwitchPortStatuses(
+    serial
+)
+
+#pprint(response)
+
+for port in response:
+    pprint(port['portId'])
+    pprint(port['status'])
+    print()
